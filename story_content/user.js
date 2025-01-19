016c9b87de4e773e3084c7e6aba3d6e1e81cf5f9
anitazhr20 <anitazahra2004@gmail.com>
2025-01-19 23:02:24 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ZkSnR4w9Gt":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+
+audio.src="backsound.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume=0.3;
+}
+
